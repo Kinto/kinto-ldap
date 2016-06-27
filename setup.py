9c4d1d5af724e6e7b174d288adf5d223d067b011
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-06-27 17:07:34 +0200

    Back to development: 0.2.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ENTRY_POINTS = {}
 
 
 setup(name='kinto-ldap',
-      version='0.1.0',
+      version='0.2.0.dev0',
       description='LDAP support for Kinto',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
